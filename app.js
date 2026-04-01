bfa32993228f5f08014851ddd1b52eb54a0c3cb8
Hammad <official.hammadansari@gmail.com>
2026-04-01 21:13:39 +0545

    First comit

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ require('./config/db');
 app.use(express.static('public'));
 app.use(express.json());
 
-app.use('/', require('./routes/authRoutes'));
-app.use('/', require('./routes/profileRoutes'));
-app.use('/', require('./routes/logRoutes'));
+app.use('/', require('./api/authRoutes'));
+app.use('/', require('./api/profileRoutes'));
+app.use('/', require('./api/logRoutes'));
 
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
